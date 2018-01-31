03fda33692bf15d412a839daf33c43e31b1d790c
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-01-31 16:41:18 +0530

    Update 8-10.c

--- 8-10.c
+++ 8-10.c
@@ -1,24 +1,22 @@
 #include<stdio.h>
 void main()
 {
-int a[5],i,r=0,n;
+int a[5],i,r=0,n,num;
 printf("Enter a number");
 scanf("%d",&a[i]);
+printf("Enter the Number of DIgits in the given number");
+scanf("%d",&num);
 if(a[i]!=0)
 {
-for(i=0;i<=5;i++)
+for(i=0;i<num;i++)
 {
-n=a%10;
+n=a[i]%10;
 r=r+n;
 n=n/10;
 printf("%d",n);
 if(n%2!=0)
 {
 printf("%d",n);
-}
-else
-{
-printf("NO ODD NUMBERS");
 }
 }
 }
