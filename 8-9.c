0532faebe6888be9fe6fd9b8a7f58a53eb2ee899
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-01-31 16:44:50 +0530

    Update 8-9.c

--- 8-9.c
+++ 8-9.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 void main()
 {
-int a,b,c;
+int a,b,c,d;
 printf("Enter 2 numbers");
 scanf("%d\t%d",&a,&b);
 if((a!=0)&&(b!=0))
 {
 c=a*b;
+for(i=0;i<=a;i++)
+{
+  if(c==(i*i))
+  {
+    printf("It is a Square");
+  }
+  else
+  {
+    printf("It is not a square");
+  }
+}
+}
